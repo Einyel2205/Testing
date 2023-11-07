0b86484754afb2a1af04ae03e0b8f32a4015badd
LuisVelez1 <126474251+LuisVelez1@users.noreply.github.com>
2023-11-06 19:32:12 -0500

    cambio de hello correcto

--- concatenarStrings/concatenateStrings.test.js
+++ concatenarStrings/concatenateStrings.test.js
@@ -1,6 +1,6 @@
 const {concatenateStrings} = require('./concatenateStrings');
 test('Contatenar dos strings', () =>{
-    expect(concatenateStrings('Hello, ', 'world')).toBe('Hell, world');
+    expect(concatenateStrings('Hello, ', 'world')).toBe('Hello, world');
 });
     
 test('Lanza un error si alguno de los argumentos no es una cadena', () => {
